75c3768dc3906be6bd79078da323e03aed4f42bc
chenchenghao <459105408@qq.com>
2018-01-25 10:23:41 +0800

    服务消费(rest+ribbon)

---
+++ springclouddemo02/service-hi/src/main/java/com/cch/demo02/ServiceHitApplication.java
@@ -0,0 +1,14 @@
+package com.cch.demo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceHitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHitApplication.class, args);
+	}
+}
---
+++ springclouddemo02/service-hi/src/main/java/com/cch/demo02/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.cch.demo02.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 459105408@qq.com
+ * 2018-01-24 15:15.
+ */
+@RestController
+public class TestController {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+}
